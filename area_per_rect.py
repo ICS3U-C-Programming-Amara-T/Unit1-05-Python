176322b7c4598b7a201a4f9d104c1b4c8a7ca61e
Amara Tie <amara.tie@stu.ocsb.ca>
2025-02-18 18:06:19 +0000

    Added

---
+++ area_per_rect.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created By: Amara Tie
+
+# Date: Febuary 18, 2025
+
+# This code calculates area and perimeter.
+
+def main():
+    # this function calculates the area and perimeter
+    print("For a rectangle with the dimension of 2cm by 7cm:")
+    print("2cm x 7cm")
+    print()
+    print("Area is {}cm^2".format(5*3))
+    print("Perimeter is: {}cm".format(2 * (5+3))) 
+
+
+if __name__ == "__main__":
+    main()
